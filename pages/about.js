88b5e897e173dd65975016dc125437203e2bd05d
sachhin <sachin.maurya@tothenew.com>
2019-06-18 22:31:29 +0530

    Link added

--- pages/about.js
+++ pages/about.js
@@ -1,5 +1,8 @@
+import Navbar from "../component/navbar";
+
 const Index = ()=>(
     <div>
+        <Navbar/>
         <h1>About page</h1>
     </div>
 )
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
+import Navbar from '../component/navbar';
+
 const Index = ()=>(
     <div>
+        <Navbar/>
         <h1>Welcome to NextjsApp Index page</h1>
     </div>
 )
